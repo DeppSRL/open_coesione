8b70375c5bea89611c2214bd3ca35a96a7d3f5f4
Guglielmo <guglielmo.celata@gmail.com>
2012-06-05 23:56:39 +0200

    geo views added to show provinces and municipalities

--- localita/urls.py
+++ localita/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, url
 from django.views.generic.base import TemplateView
-from localita.views import RegionDetailView, RegionListView
+from localita.views import RegionDetailView, RegionListView, ProvinceListView, ProvinceDetailView, MunicipalityDetailView
 
 urlpatterns = patterns('',
    url(r'^$', TemplateView.as_view(template_name='localita/leaflet.html')),
    url(r'^regioni.json$',
-       RegionListView.as_view(), name='json_region_url'),
-   url(r'^regione/(?P<cod_reg>[^/]+).json$',
-       RegionDetailView.as_view(), name='json_region_url'),
+       RegionListView.as_view(), name='json_province_list_url'),
+   url(r'^province.json$',
+       ProvinceListView.as_view(), name='json_region_list_url'),
+   url(r'^regione/(?P<cod_reg>[^/]+)/(?P<type>[^/]).json$',
+       RegionDetailView.as_view(), name='json_region_details_url'),
+   url(r'^provincia/(?P<cod_prov>[^/]+).json$',
+       ProvinceDetailView.as_view(), name='json_province_details_url'),
+   url(r'^comune/(?P<cod_com>[^/]+).json$',
+       MunicipalityDetailView.as_view(), name='json_municipality_details_url'),
 )
-
--- localita/views.py
+++ localita/views.py
@@ -4,12 +4,19 @@ from django.views.generic.base import TemplateView
 from vectorformats.Formats import Django, GeoJSON
 
 
+class ProvinceListView(TemplateView):
+    def render_to_response(self, context, **response_kwargs):
+        l = Localita.objects.filter(territorio='P')
+
+        djf = Django.Django(geodjango="geom", properties=['nome', 'cod_prov'])
+        geoj = GeoJSON.GeoJSON()
+        return HttpResponse(geoj.encode(djf.decode(l)), mimetype='application/json')
+
 class RegionListView(TemplateView):
     def render_to_response(self, context, **response_kwargs):
-
         l = Localita.objects.filter(territorio='R')
 
-        djf = Django.Django(geodjango="geom", properties=['nome'])
+        djf = Django.Django(geodjango="geom", properties=['nome', 'cod_reg'])
         geoj = GeoJSON.GeoJSON()
         return HttpResponse(geoj.encode(djf.decode(l)), mimetype='application/json')
 
@@ -17,10 +24,37 @@ class RegionListView(TemplateView):
 class RegionDetailView(TemplateView):
 
     def render_to_response(self, context, **response_kwargs):
-
         params = context['params']
         cod_reg = params['cod_reg']
-        l = Localita.objects.filter(cod_reg=cod_reg, territorio='C')
+        type = params['type']
+        if type != 'C' and type != 'P':
+            raise Exception("Wrong type %s. C or P must be specified." % type)
+
+        l = Localita.objects.filter(cod_reg=cod_reg, territorio=type)
+
+        djf = Django.Django(geodjango="geom", properties=['nome', 'cod_prov', 'cod_com'])
+        geoj = GeoJSON.GeoJSON()
+        return HttpResponse(geoj.encode(djf.decode(l)), mimetype='application/json')
+
+
+class ProvinceDetailView(TemplateView):
+
+    def render_to_response(self, context, **response_kwargs):
+        params = context['params']
+        cod_prov = params['cod_prov']
+
+        l = Localita.objects.filter(cod_prov=cod_prov, territorio='C')
+
+        djf = Django.Django(geodjango="geom", properties=['nome', 'cod_com'])
+        geoj = GeoJSON.GeoJSON()
+        return HttpResponse(geoj.encode(djf.decode(l)), mimetype='application/json')
+
+class MunicipalityDetailView(TemplateView):
+    def render_to_response(self, context, **response_kwargs):
+        params = context['params']
+        cod_com = params['cod_com']
+
+        l = Localita.objects.filter(cod_com=cod_com)
 
         djf = Django.Django(geodjango="geom", properties=['nome'])
         geoj = GeoJSON.GeoJSON()
