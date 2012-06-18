617808f15969a7d953b9256ff3ccd054e53d8435
Guglielmo <guglielmo.celata@gmail.com>
2012-06-18 14:38:03 +0200

    fabfile added back to git synced files

---
+++ fabfile/static.py
@@ -0,0 +1,16 @@
+from fabric.api import *
+
+from venv import run_venv
+
+import os
+
+## Static files management
+@task
+@roles('web')
+def collect_files():
+    require('static_root','virtualenv_root', provided_by=('production'))
+    # safety checks
+    assert env.static_root.strip() != '' and env.static_root.strip() != '/'
+    # Before Django 1.4 we don't have the ``--clear`` option to ``collectstatic``
+    run('rm -rf %(static_root)s/*' % env)
+    run_venv('django-admin.py collectstatic -v 0 --noinput --settings=%(settings)s' % env)
