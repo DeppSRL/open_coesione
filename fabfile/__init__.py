62e172418f3fa47fd617c956479bdb55f91c1f9d
Guglielmo <guglielmo.celata@gmail.com>
2012-06-19 15:32:26 +0200

    started implementing the fabfile for this project

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,159 +1,97 @@
 ## -*- mode: python -*-
 """
-Starter fabfile for deploying a Django-powered web application.
+Fabric script (a.k.a. "fabfile") to automate the deployment process
+of an instance of the Open Coesione web application.
 
-All the settings marked with ``CHANGEME`` MUST be changed to reflect
-project-specific setup.  Other settings MAY be changed, but their values should be
-generic enough to provide for sensible defaults.
+Deployment on both staging & production machine(s) is supported:
+
+* To deploy on the staging server(s), run the shell command ``fab staging deploy``
+* To deploy on the production server(s), run the shell command ``fab production deploy``
+
+The first time you perform a deploy on a given machine (or set of machines), you MUST
+use the command  `fab (staging|production) initial_deploy`` instead, which takes care
+of performing one-time initialization tasks.
+
+In order to customize the deployment procedure to suit your specific needs,
+you can control the behaviour of this script by setting up the provided configuration files
+(``conf_staging.py`` and ``conf_production.py`` for staging and production deployment,
+respectively).
 """
-from fabric import utils
-from fabric.api import *
-from fabric.context_managers import cd, lcd, settings
+from fabric.utils import abort
+from fabric.api import env, execute, hide, require, roles, run, settings, sudo, task
 
-import code, database as db, venv, static, webserver, solr, tasks
+import code, database as db, venv, static, webserver, solr, provision
 
 import os
 
-# Python interpreter executable to use on virtualenv creation
-PYTHON_BIN = 'python' #pyhton 2.7
-PYTHON_PREFIX = '' # e.g. ``/usr``, ``/usr/local``; leave empty for default.
-PYTHON_FULL_PATH = "%s/bin/%s" % (PYTHON_PREFIX, PYTHON_BIN) if PYTHON_PREFIX else PYTHON_BIN
 
-# exclude patterns for ``rsync`` invocations
-RSYNC_EXCLUDE = ( 
-    ##CHANGEME!
-    '*~',
-    '.git',
-    '.gitignore',
-    '.idea',
-    '.DS_Store',
-    '*.pyc',
-    '*.sample',
-    '*.eml',
-    'docs/',
-    '*.markdown',
-    '*.gz',
-    'fabfile',
-    'project_site',
-    'apache/*',
-    'import_tmp/*',
-    'solr',
-    'log',
-    'solr.example',
-    'open_coesione/settings_*.py',
-    'open_coesione/urls_*.py',
-    'sitestatic',
-    'media',
-    '*.sqlite',
-    '*.db',
-    '*.db.old',
-    'manage.py',
-    'fixtures/*',
-    'GIS/tiles/*',
-    'dati/*',
-    'splitcsv2.py',
-    'smtp_sink_server.py',
-)
-
-## TODO: these constants should be read from an external configuration file
-# the name of the project managed by this fabfile
-PROJECT_NAME = 'open_coesione'
-# a unique identifier for this web application instance
-# usually is set to the primary domain from which the web application is accessed
-APP_DOMAIN = 'stagingcoesione.deppsviluppo.org'
-# filesystem location of project's files on the local machine
-LOCAL_PROJECT_ROOT = '/Users/guglielmo/Workspace/open_coesione'
-
-env.project = PROJECT_NAME
-env.app_domain = APP_DOMAIN
-env.local_project_root = LOCAL_PROJECT_ROOT
-env.rsync_exclude = RSYNC_EXCLUDE
-env.python = PYTHON_FULL_PATH
 ## Environment-specific setup
-
 @task
 def staging():
-    """ Use staging environment on remote host"""
-    env.environment = 'staging'
-    ## TODO: these constants should be read from an external configuration file
-    # the system user (on the server machine) used for managing websites
-    WEB_USER = 'webmaster'
-    # the parent directory of domain-specific directories (on the server machine)
-    WEB_ROOT = '/home/' #I keep it simple, here
-    # the root directory for domain-specific files (on the server machine)
-    DOMAIN_ROOT = os.path.join(WEB_ROOT, env.app_domain)
-    # the root directory of application-specific Python virtual environment (on the server machine)
-    VIRTUALENV_ROOT = os.path.join(DOMAIN_ROOT, 'private', 'venv')
-    # the root directory for project-specific files (on the server machine)
-    PROJECT_ROOT = os.path.join(DOMAIN_ROOT, 'private', 'python')
-    # the root directory for application-specific Python code (on the server machine)
-    CODE_ROOT = os.path.join(PROJECT_ROOT, env.project) ##CHANGEME!
-    # import path of Django settings file for the staging environment
-    DJANGO_SETTINGS_MODULE = '%(project)s.settings_staging' % env
-    # Directory where static files should be collected.  This MUST equal the value
-    # of ``STATIC_ROOT`` attribute of the Django settings module used on the server.
-    STATIC_ROOT =  os.path.join(DOMAIN_ROOT, 'public', 'static') ## CHANGEME!
-
+    """
+    Deploys OpenMunicipio to the staging server.
+    """
+    # import staging's conf module
+    import conf_staging as conf
     ## set up Fabric global environment dictionary
-    env.web_user = WEB_USER
-    env.web_root = WEB_ROOT
-    env.domain_root = DOMAIN_ROOT
-    env.virtualenv_root = VIRTUALENV_ROOT
-    env.project_root = PROJECT_ROOT
-    env.code_root = CODE_ROOT
-    env.settings = DJANGO_SETTINGS_MODULE
-    env.static_root = STATIC_ROOT
-
+    env.environment = 'staging'
+    env.server = conf.SERVER_MACHINE
+    env.project = conf.PROJECT_NAME
+    env.app_domain = conf.APP_DOMAIN
+    env.local_repo_root = conf.LOCAL_REPO_ROOT
+    env.local_project_root = conf.LOCAL_PROJECT_ROOT
+    env.rsync_exclude = conf.RSYNC_EXCLUDE
+    env.python = conf.PYTHON_FULL_PATH
+    env.web_user = conf.WEB_USER
+    env.web_root = conf.WEB_ROOT
+    env.domain_root = conf.DOMAIN_ROOT
+    env.virtualenv_root = conf.VIRTUALENV_ROOT
+    env.project_root = conf.PROJECT_ROOT
+    env.settings = conf.DJANGO_SETTINGS_MODULE
+    env.static_root = conf.STATIC_ROOT
+    # Tomcat
+    env.tomcat_user = conf.TOMCAT_USER
+    env.tomcat_controller = conf.TOMCAT_CONTROLLER
+    env.catalina_home = conf.CATALINA_HOME
+    # Solr
+    env.solr_download_link = conf.SOLR_DOWNLOAD_LINK
+    env.solr_install_dir = conf.SOLR_INSTALL_DIR
+    env.solr_home = conf.SOLR_HOME
+    # role definitions
     env.roledefs = {
-        'web': ['%(web_user)s@ovhb2' % env],
-        'db': ['dba@ovhb2'],
+        'admin': ['root@%(server)s' % env],
+        'web': ['%(web_user)s@%(server)s' % env],
+        'db': ['dba@%(server)s' % env],
+        'solr': ['solr@%(server)s' % env],
         }
-
 
 @task
 def production():
-    """ Use staging environment on remote host"""
+    """
+    Deploys OpenMunicipio to the production server.
+    """
     env.environment = 'production'
-    ## TODO: these constants should be read from an external configuration file
-    # the system user (on the server machine) used for managing websites
-    WEB_USER = 'webmaster'
-    # the parent directory of domain-specific directories (on the server machine)
-    WEB_ROOT = '/home/' #I keep it simple, here
-    # the root directory for domain-specific files (on the server machine)
-    DOMAIN_ROOT = os.path.join(WEB_ROOT, env.app_domain)
-    # the root directory of application-specific Python virtual environment (on the server machine)
-    VIRTUALENV_ROOT = os.path.join(DOMAIN_ROOT, 'private', 'venv')
-    # the root directory for project-specific files (on the server machine)
-    PROJECT_ROOT = os.path.join(DOMAIN_ROOT, 'private', 'python')
-    # the root directory for application-specific Python code (on the server machine)
-    CODE_ROOT = os.path.join(PROJECT_ROOT, env.project) ##CHANGEME!
-    # import path of Django settings file for the staging environment
-    DJANGO_SETTINGS_MODULE = '%(project)s.settings_production' % env
-    # Directory where static files should be collected.  This MUST equal the value
-    # of ``STATIC_ROOT`` attribute of the Django settings module used on the server.
-    STATIC_ROOT =  os.path.join(DOMAIN_ROOT, 'public', 'static') ## CHANGEME!
+    abort('Production deployment not yet implemented.')
 
-    ## set up Fabric global environment dictionary
-    env.web_user = WEB_USER
-    env.web_root = WEB_ROOT
-    env.domain_root = DOMAIN_ROOT
-    env.virtualenv_root = VIRTUALENV_ROOT
-    env.project_root = PROJECT_ROOT
-    env.code_root = CODE_ROOT
-    env.settings = DJANGO_SETTINGS_MODULE
-    env.static_root = STATIC_ROOT
 
-    env.roledefs = {
-    'web': ['%(web_user)s@XXX' % env],
-    'db': ['dba@XXX'],
-    }
- 
 ## Macro-tasks
+
 @task
-@roles('web')
+def setup_platform():
+    """
+    Takes care of all preliminay tasks needed to setup the OS of the
+    target machine(s) in order for the deploy of an OpenMunicipio instance
+    to be possible.
+    """
+    require('environment', provided_by=('staging', 'production'))
+    # setup Solr (include Tomcat's setup)
+    execute(provision.setup_solr)
+    # execute(provision.setup_postgres)
+
+@task
 def initial_deploy():
     """
-    Deploy the web application to remote server(s) **for the first time**
+    Deploy the web application to remote server(s) **for the first time**.
 
     The first deployment procedure may differ from subsequent ones,
     since some initialization tasks have to be performed only once.
@@ -162,9 +100,10 @@ def initial_deploy():
     * fake South migrations
     * ..
     """
-    require('environment', provided_by=('production', 'staging'))
+    require('environment', provided_by=('staging', 'production'))
     env.initial_deploy = True
-    deploy()        
+    execute(deploy)
+
 
 @task
 @roles('web')
@@ -172,24 +111,52 @@ def deploy():
     """
     Deploy the web application to remote server(s)
     """
-    require('environment', provided_by=('production', 'staging'))
-    ## TODO: early initialization steps go here  
-    if env.get('initial_deploy'):
-        code.copy_website_skeleton()
+    require('environment', provided_by=('staging', 'production'))
+    with hide('commands'):
+    ## one-time initialization steps go here
+        if env.get('initial_deploy'):
+            # create the initial filesystem layout
+            execute(code.make_website_skeleton)
+            # add a new core for this OpenMunicipio instance
+            execute(solr.add_new_core)
+            ## tasks to be performed each time the application is deployed on the server
+            # sanity check
+        with hide('everything'):
+            if run('test -d %(domain_root)s' % env).failed:
+                abort("It seems that the root dir for this OpenMunicipio instance has not been created, yet.")
+        with settings(warn_only=True):
+            execute(webserver.stop)
+            # update Django project's files
+        execute(code.update_project)
+        # update external dependencies
+        execute(venv.update_requirements)
+        # update DB
+        execute(db.update)
+        # update Solr conf
+        execute(solr.update_core_conf)
+        # update Solr index
+        execute(solr.update_index)
+        # collect static files
+        execute(static.collect_files)
+        # clear webserver's log when deploying to the staging server
+        if env.environment == 'staging': execute(webserver.clear_logs)
+        # update webserver configuration
+        execute(webserver.update_conf)
+        execute(webserver.start)
+        # adjust filesystem permissions
+        execute(adjust_permissions)
 
-    code.update()
-    venv.update_requirements()
-    db.update()
-    webserver.clear_logs()
 
-    webserver.restart()
-
-
+@roles('admin')
 def adjust_permissions():
     """
     Adjust filesystem permissions after completing the deployment process.
     """
-    require('web_user', 'domain_root', provided_by=('production', 'staging'))
-    sudo('chown -R www-data:sudo %(domain_root)s' % env)
-
-
+    require('web_user', 'domain_root', provided_by=('staging', 'production'))
+    sudo('chown -R %(web_user)s:www-data %(domain_root)s' % env)
+    ## Only needed for SQLite DBs
+    # SQLite needs write access to the folder containing the DB file
+    sudo('chmod g+w %(domain_root)s/private' % env)
+    # SQLite need write access to the DB file itself
+    db_file = os.path.join(env.domain_root, 'private', 'db.sqlite')
+    sudo('chmod g+w %s' % db_file)
--- fabfile/code.py
+++ fabfile/code.py
@@ -1,6 +1,5 @@
-from fabric.api import *
-from fabric.context_managers import cd, lcd, settings
-from fabric import utils
+from fabric.api import cd, env, hide, lcd, put, require, roles, run, sudo, task
+from fabric.utils import abort, fastprint
 from fabric.contrib import files, console
 from fabric.contrib.project import rsync_project
 
@@ -10,76 +9,93 @@ import os
 
 ## Application code management
 @task
-def copy_website_skeleton():
+@roles('web')
+def make_website_skeleton():
     """
-    Create a skeleton directory tree for the new website.
+    Creates a skeleton directory tree for the new web app.
 
-    Include setup of suitable filesystem permissions.
+    Includes setup of suitable filesystem permissions.
     """
     require('web_root', 'app_domain', provided_by=('staging', 'production'))
-    with cd(env.web_root):
-        if files.exists(env.app_domain):
-            utils.abort("Directory %s already exists on the remote machine" %
-                        os.path.join(env.web_root, env.app_domain))
-        run('mkdir %(app_domain)s' % env)
-        run('mkdir -p %(app_domain)s/backup' % env)
-        run('mkdir -p %(app_domain)s/log' % env)
-        run('mkdir -p %(app_domain)s/private' % env)
-        run('mkdir -p %(app_domain)s/private/python' % env)
-        run('mkdir -p %(app_domain)s/private/venv' % env)
-        run('mkdir -p %(app_domain)s/public' % env)
-        run('mkdir -p %(app_domain)s/public/media' % env)
-        run('mkdir -p %(app_domain)s/public/static' % env)
+    fastprint("Setting up initial filesystem layout for this Open Coesione instance..." % env, show_prefix=True)
+    with hide('stdout', 'running'):
+        with cd(env.web_root):
+            if files.exists(env.app_domain):
+                abort("Directory %s already exists on the remote machine" %
+                      os.path.join(env.web_root, env.app_domain))
+                ## create filesystem skeleton
+            run('mkdir -p %(app_domain)s/backup' % env)
+            run('mkdir -p %(app_domain)s/log' % env)
+            run('mkdir -p %(app_domain)s/private/%(project)s' % env)
+            run('mkdir -p %(app_domain)s/private/venv' % env)
+            run('mkdir -p %(app_domain)s/private/apache' % env)
+            run('mkdir -p %(app_domain)s/public/media' % env)
+            run('mkdir -p %(app_domain)s/public/static' % env)
+            ## setup permissions
+            # webserver user needs read access to (nearly) every file within its domain
+            sudo('chown -R %(web_user)s:www-data %(app_domain)s' % env)
+            sudo('chmod -R 2750 %(app_domain)s' % env)
+            # webserver user needs RW access to the directory holding files uploaded by clients
+            sudo('chmod g+w %(app_domain)s/public/media' % env)
+    fastprint(" done." % env, end='\n')
 
 
 @task
 @roles('web')
-def update():
-    """Transfer application code to remote host"""
-    require('project_root', 'code_root', provided_by=('production'))
+def update_project():
+    """
+    Updates Django project's files  on the remote host.
+    
+    Works by syncing contents of the project dir on the local machine
+    with the corresponding one on the remote staging/production server(s).
+    
+    Therefore, to, deploy a given version of the Django project, all you need 
+    to do is to checkout the chosen version on the local machine before starting
+    the deploy process.
+    
+    Notice that you can prevent some files from being synced to the server via the
+    ``RSYNC_EXCLUDE`` config option.
+    """
+    require('project_root', provided_by=('staging', 'production'))
     if env.environment == 'production':
         if not console.confirm('Are you sure you want to deploy to the production server(s)?',
                                default=False):
-            utils.abort('Production deployment aborted.')
-    # defaults rsync options:
-    # -pthrvz
-    # -p preserve permissions
-    # -t preserve times
-    # -h output numbers in a human-readable format
-    # -r recurse into directories
-    # -v increase verbosity
-    # -z compress file data during the transfer
-    extra_opts = '--omit-dir-times'
-    rsync_project(
-        remote_dir = env.project_root,
-        local_dir = env.local_project_root + os.path.sep,
-        exclude=env.rsync_exclude,
-        delete=True,
-        extra_opts=extra_opts,
-    )
-    
-    with lcd(os.path.join(env.local_project_root, env.project)):
-        with cd(env.code_root):
-            # update Django settings module
-            settings_file = 'settings_%(environment)s.py' % env
-            put(settings_file, settings_file, mode=0644)
-
-    #TODO: deploy on nginx is different
-    with lcd(os.path.join(env.local_project_root, 'apache')):
-        with cd(os.path.join(env.project_root, 'apache')):
-            source = '%(environment)s.wsgi' % env
-            dest = 'django.wsgi'
-            put(source, dest, mode=0644)
-
-            source = 'httpd.conf.%(environment)s' % env
-            dest = 'httpd.conf'
-            put(source, dest, mode=0644)
-
-            source = 'vhost.conf.%(environment)s' % env
-            dest = 'vhost.conf'
-            put(source, dest, mode=0644)
-
-    # trigger code reloading
-    # TODO: deploy on nginx is different
-#    touch_WSGI_script()
-
+            abort('Production deployment aborted.')
+    with hide('stdout', 'running'):
+        fastprint("Updating Django project files..." % env, show_prefix=True)
+        # defaults rsync options:
+        # -pthrvz
+        # -p preserve permissions
+        # -t preserve times
+        # -h output numbers in a human-readable format
+        # -r recurse into directories
+        # -v increase verbosity
+        # -z compress file data during the transfer
+        extra_opts = '--omit-dir-times'
+        rsync_project(
+            remote_dir = env.project_root,
+            local_dir = env.local_project_root + os.path.sep,
+            exclude=env.rsync_exclude,
+            delete=True,
+            extra_opts=extra_opts,
+            )
+        fastprint(" done." % env, end='\n')
+        fastprint("Updating settings & URLconfs modules..." % env, show_prefix=True)
+        with lcd(env.local_project_root):
+            with cd(env.project_root):
+                # update Django settings module
+                settings_file = 'settings_%(environment)s.py' % env
+                put(settings_file, settings_file, mode=0644)
+                # update Django main URLconf module
+                urls_file = 'urls_%(environment)s.py' % env
+                put(urls_file, urls_file, mode=0644)
+        fastprint(" done." % env, end='\n')
+        fastprint("Updating WSGI script..." % env, show_prefix=True)
+        with lcd(os.path.join(env.local_repo_root, 'apache')):
+            with cd(os.path.join(env.domain_root, 'private', 'apache')):
+                source = '%(environment)s.wsgi' % env
+                dest = 'django.wsgi'
+                put(source, dest, mode=0644)
+        fastprint(" done." % env, end='\n')
+        # trigger code reloading
+    touch_WSGI_script()
