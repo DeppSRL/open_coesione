b9459ca96235e8a90f2a117fb1db4701a8191aaf
Guglielmo <guglielmo.celata@gmail.com>
2012-07-06 12:14:44 +0200

    populationimport script that imports and computes population data to Territorio model implemented

--- territori/management/commands/populationimport.py
+++ territori/management/commands/populationimport.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.aggregates import Sum
 from django.db.utils import DatabaseError
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -50,33 +51,67 @@ class Command(BaseCommand):
         elif verbosity == '3':
             self.logger.setLevel(logging.DEBUG)
 
+        self.logger.info("Reset dei dati a zero")
+        Territorio.objects.update(
+            popolazione_totale=0,
+            popolazione_maschile=0,
+            popolazione_femminile=0
+        )
+
         self.logger.info("Inizio import da %s" % self.csv_file)
 
 
+        self.logger.info("Aggiornamento comuni")
         for r in self.csv_reader:
             c = self.csv_reader.reader.line_num - 1
 
             # determina territorio a partire da codice comune e regione
             try:
-                territorio = Territorio.objects.get(cod_reg=int(r['CODICE REGIONE']), cod_com=int(r['CODICE COMUNE']))
-                self.logger.debug(
-                    "{0} - Territorio: {1.denominazione} ({1.cod_reg}-{1.cod_com})".format(c, territorio)
-                )
+                territorio = Territorio.objects.get(cod_reg=int(r['CODICE REGIONE'][0:2]), cod_com=int(r['CODICE COMUNE']))
             except ObjectDoesNotExist:
                 self.logger.warning("{0} - Territorio non trovato: {1}-{2}, skipping".format(c, r['CODICE REGIONE'], r['CODICE COMUNE']))
                 continue
 
-            territorio.popolazione_totale = int(r['TOT'])
-            territorio.popolazione_maschile = int(r['MASCHI'])
-            territorio.popolazione_femminile = int(r['FEMMINILE'])
+            territorio.popolazione_totale += int(r['TOTALE'])
+            territorio.popolazione_maschile += int(r['MASCHI'])
+            territorio.popolazione_femminile += int(r['FEMMINE'])
             territorio.save()
-            self.logger.debug(
-                "popolazione: {0.popolazione_totale} M:{0.popolazione_maschile} F:{0.popolazione_femminile}".format(territorio)
+            self.logger.info(
+                u"{0} - {1.denominazione} - T:{1.popolazione_totale} M:{1.popolazione_maschile} F:{1.popolazione_femminile}".format(c, territorio)
             )
 
-        self.logger.info(u"Comuni aggiornati")
 
+        self.logger.info("Aggiornamento province")
+        for t in Territorio.objects.filter(territorio=Territorio.TERRITORIO.P):
+            pop = Territorio.objects.filter(territorio='C', cod_prov=t.cod_prov).\
+                aggregate(
+                    tot=Sum('popolazione_totale'),
+                    m=Sum('popolazione_maschile'),
+                    f=Sum('popolazione_femminile')
+                )
+            t.popolazione_totale += int(pop['tot'])
+            t.popolazione_maschile += int(pop['m'])
+            t.popolazione_femminile += int(pop['f'])
+            t.save()
+            self.logger.info(
+                u"{0.denominazione} - T:{0.popolazione_totale} M:{0.popolazione_maschile} F:{0.popolazione_femminile}".format(t)
+            )
 
+        self.logger.info("Aggiornamento regioni")
+        for t in Territorio.objects.filter(territorio=Territorio.TERRITORIO.R):
+            pop = Territorio.objects.filter(territorio='P', cod_reg=t.cod_reg).\
+            aggregate(
+                tot=Sum('popolazione_totale'),
+                m=Sum('popolazione_maschile'),
+                f=Sum('popolazione_femminile')
+            )
+            t.popolazione_totale += int(pop['tot'])
+            t.popolazione_maschile += int(pop['m'])
+            t.popolazione_femminile += int(pop['f'])
+            t.save()
+            self.logger.info(
+                u"{0.denominazione} - T:{0.popolazione_totale} M:{0.popolazione_maschile} F:{0.popolazione_femminile}".format(t)
+            )
 
         self.logger.info("Fine")
 
