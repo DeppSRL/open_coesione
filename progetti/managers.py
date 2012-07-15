9bb85a75fda0a86dd557339df21c62398afdf4e0
Guglielmo <guglielmo.celata@gmail.com>
2012-07-15 10:29:06 +0200

    aggregations with values need unique fields, or less values are taken

--- progetti/managers.py
+++ progetti/managers.py
@@ -68,10 +68,10 @@ class ProgettiQuerySet(models.query.QuerySet):
         return self.filter(soggetto_set__pk=soggetto.pk)
 
     def totale_costi(self, territorio=None, tema=None, tipo=None,classificazione=None, soggetto=None):
-        return float(sum([l[0] for l in self.totali(territorio, tema, tipo,classificazione, soggetto).filter(fin_totale_pubblico__isnull=False).values_list('fin_totale_pubblico')]) or 0.0)
+        return float(sum([l['fin_totale_pubblico'] for l in self.totali(territorio, tema, tipo,classificazione, soggetto).filter(fin_totale_pubblico__isnull=False).values('codice_locale', 'fin_totale_pubblico')]) or 0.0)
 
     def totale_pagamenti(self, territorio=None, tema=None, tipo=None,classificazione=None, soggetto=None):
-        return float(sum([l[0] for l in self.totali(territorio, tema, tipo,classificazione, soggetto).filter(pagamento__isnull=False).values_list('pagamento')]) or 0.0)
+        return float(sum([l['pagamento'] for l in self.totali(territorio, tema, tipo,classificazione, soggetto).filter(pagamento__isnull=False).values('codice_locale', 'pagamento')]) or 0.0)
 
     def totale_progetti(self, territorio=None, tema=None, tipo=None,classificazione=None, soggetto=None):
         return self.totali(territorio, tema, tipo,classificazione, soggetto).count()
