0052ad26a442547e46819d39ba89f90f5a16c401
Guglielmo <guglielmo.celata@gmail.com>
2013-03-08 00:16:37 +0100

    extract big soggetti can be called in curl mode, to extract curl requests directly

--- open_coesione/management/commands/extract_big_soggetti.py
+++ open_coesione/management/commands/extract_big_soggetti.py
@@ -11,21 +11,31 @@ class Command(BaseCommand):
     """
     Extracts and print soggetti over the specified treshold
     """
-    help = "Import data from csv"
+    help = "Extract soggetti with more than treshold progetti"
 
     option_list = BaseCommand.option_list + (
         make_option('--treshold',
                     dest='treshold',
                     default='100',
                     help='Ectract only soggetti with a number of progetti greather than this'),
+        make_option('--curl',
+                    dest='curl',
+                    action='store_true',
+                    default=False,
+                    help='Output in a curl ready format'),
     )
 
     def handle(self, *args, **options):
         self.treshold = options['treshold']
+        self.curl = options['curl']
 
         grandi_soggetti = Soggetto.objects.all().annotate(n=Count('progetto')).filter(n__gt=self.treshold).order_by('-n')
         for s in grandi_soggetti:
-            print "%s;%s" % (s.get_absolute_url(), s.n)
+            if self.curl:
+                print "curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'\n",\
+                      "\"http://opencoesione.gov.it/%s{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}\" >> cache_generation.log" % s.get_absolute_url()
+            else:
+                print "%s;%s" % (s.get_absolute_url(), s.n)
 
 
 
