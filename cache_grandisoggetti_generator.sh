5976c364364b9f183bf5ea47b49743aceebe7b49
Guglielmo <guglielmo.celat@gmail.com>
2013-03-10 09:15:27 +0000

    cache generator for soggetti re-written

--- cache_grandisoggetti_generator.sh
+++ cache_grandisoggetti_generator.sh
@@ -1,59 +1,58 @@
-rm cache_generation.log
-touch cache_generation.log
+rm cache_grandisoggetti_generation.log
+touch cache_grandisoggetti_generation.log
 
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-lombardia-codice-beneficiario-edma-22226-80050050154/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/miur-97429780584/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-autonoma-friuli-venezia-giulia-80014930327/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/provincia-di-firenze-direzione-formazione-01709770489/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/fondazione-enaip-lombardia-80203850153/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-puglia-80017210727/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/centro-formativo-provinciale-giuseppe-zanardelli-02481950984/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-calabria/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-del-veneto-giunta-regionale-80007580279/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/ial-innovazione-apprendimento-lavoro-lombardia-srl-impresa-sociale-80145370153/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/apprendistatofvgit-80035920323/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/provincia-di-torino-01907990012/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/provincia-di-lucca-80001210469/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/assessorato-regionale-dellistruzione-e-della-formazione-professionale-dipartimento-regionale-dellistruzione-e-della-formazione-professionale-80012000826/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-abruzzo-80003170661/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/obiettivo-lavoro-agenzia-per-il-lavoro-societa-per-azioni-05510281008/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/provincia-di-imperia/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-autonoma-valle-daosta-80002270074/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-autonoma-della-sardegna-80002870923/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/apprendistiformazione2-94011720300/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/maw-men-at-work-spa-03472640170/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/provincia-di-ancona-00369930425/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-basilicata-80002950766/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/regione-umbria-80000130544/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/fondazione-luigi-clerici-80037690155/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/cesvip-lombardia-centro-sviluppo-piccola-e-media-impresa-societa-cooperativa-05468500961/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/umana-spa-05391311007/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
-curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)'
-"http://opencoesione.gov.it//soggetti/adecco-italia-spa-13366030156/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_generation.log
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-lombardia-codice-beneficiario-edma-22226-80050050154/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/miur-97429780584/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-autonoma-friuli-venezia-giulia-80014930327/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/provincia-di-firenze-direzione-formazione-01709770489/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/fondazione-enaip-lombardia-80203850153/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-puglia-80017210727/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/centro-formativo-provinciale-giuseppe-zanardelli-02481950984/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-calabria/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-del-veneto-giunta-regionale-80007580279/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/ial-innovazione-apprendimento-lavoro-lombardia-srl-impresa-sociale-80145370153/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/apprendistatofvgit-80035920323/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/provincia-di-torino-01907990012/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/provincia-di-lucca-80001210469/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/assessorato-regionale-dellistruzione-e-della-formazione-professionale-dipartimento-regionale-dellistruzione-e-della-formazione-professionale-80012000826/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-abruzzo-80003170661/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/obiettivo-lavoro-agenzia-per-il-lavoro-societa-per-azioni-05510281008/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/provincia-di-imperia/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-autonoma-valle-daosta-80002270074/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-autonoma-della-sardegna-80002870923/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/apprendistiformazione2-94011720300/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/maw-men-at-work-spa-03472640170/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/provincia-di-ancona-00369930425/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-basilicata-80002950766/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/regione-umbria-80000130544/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/fondazione-luigi-clerici-80037690155/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/cesvip-lombardia-centro-sviluppo-piccola-e-media-impresa-societa-cooperativa-05468500961/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/umana-spa-05391311007/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
+
+curl -L -o/dev/null -w '%{url_effective} - %{http_code} (%{time_total}sec.)' "http://opencoesione.gov.it//soggetti/adecco-italia-spa-13366030156/{,?tematizzazione=totale_costi,?tematizzazione=totale_pagamenti,?tematizzazione=totale_progetti}" >> cache_grandisoggetti_generation.log
