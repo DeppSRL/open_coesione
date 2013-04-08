ab7168713cbf798f74753425da29d2f898ca5c75
Guglielmo <guglielmo.celat@gmail.com>
2013-04-08 22:26:10 +0000

    cost and date ranges corrected

--- progetti/urls.py
+++ progetti/urls.py
@@ -11,6 +11,7 @@ sqs = SearchQuerySet().filter(django_ct='progetti.progetto').\
         facet('natura').\
         facet('tema').\
         facet('fonte').\
+        query_facet('data_inizio', ProgettoSearchView.DATE_INTERVALS_RANGES['2013']['qrange']).\
         query_facet('data_inizio', ProgettoSearchView.DATE_INTERVALS_RANGES['2012']['qrange']).\
         query_facet('data_inizio', ProgettoSearchView.DATE_INTERVALS_RANGES['2011']['qrange']).\
         query_facet('data_inizio', ProgettoSearchView.DATE_INTERVALS_RANGES['2010']['qrange']).\
