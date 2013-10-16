ce41abc7839ed458037dd4b66ab044afe9308a46
Guglielmo <guglielmo.celata@gmail.com>
2013-10-16 12:40:11 +0200

    removed unused import

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.test import RequestFactory
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
