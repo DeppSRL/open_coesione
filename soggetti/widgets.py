7163827f92e2a1a4d0681b15cd6cf3e28578e9b2
joke2k <joke2k@gmail.com>
2013-11-11 10:04:10 +0100

    using external widgets package

--- soggetti/widgets.py
+++ soggetti/widgets.py
@@ -10,22 +10,22 @@ __author__ = 'daniele'
 class SoggettoWidget(Widget):
 
     code = "soggetto"
-    title = "Soggetto"
+    name = "Soggetto"
 
     def get_context_data(self):
         context = super(SoggettoWidget, self).get_context_data()
-        if self.get_form().is_valid():
-            cleaned_data = self.get_form().cleaned_data
+        if self.is_valid():
+            cleaned_data = self.get_data()
             context['soggetto'] = request('soggetti/{0}'.format(cleaned_data['soggetto']))
             context['top_progetti'] = request('progetti',
                                               soggetto=cleaned_data['soggetto'],
                                               order_by='-costo', page_size='5')
         return context
 
-    def build_form_fields(self, form):
+    def get_form(self):
+        form = super(SoggettoWidget, self).get_form()
         form.fields['soggetto'] = forms.CharField()
-        super(SoggettoWidget, self).build_form_fields(form)
-        del form.fields['title']
+        return form
 
     def get_initial(self):
         initial = super(SoggettoWidget, self).get_initial()
--- widgets/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-__author__ = 'joke2k'
-
-
-class WidgetForm(forms.Form):
-    pass
