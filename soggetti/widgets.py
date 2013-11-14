aaa5e1b6e1c82f343e7901b694236248d104d788
Daniele <joke2k@gmail.com>
2013-11-14 17:12:17 +0100

    soggetto widgets now extends aggregatowidget

--- soggetti/widgets.py
+++ soggetti/widgets.py
@@ -1,33 +1,43 @@
+# coding=utf-8
 from __future__ import absolute_import
-from django import forms
-from api.query import request
 from widgets.models import Widget
+from api.query import request
+from open_coesione.widgets import AggregateWidget
+from soggetti.models import Soggetto
 
 
 __author__ = 'daniele'
 
 
-class SoggettoWidget(Widget):
+class SoggettoWidget(AggregateWidget):
 
     code = "soggetto"
     name = "Soggetto"
+    title = "Soggetto"
+    API_TOPIC = 'soggetto'
+
+    _COMPONENTS = (
+        ('temi', 'Classificazione per tema'),
+        ('nature', 'Visualizza le nature'),
+        ('top_progetti', 'Visualizza i 5 progetti più finanziati'),
+        ('territori_piu_finanziati_pro_capite', 'Visualizza i 5 comuni con più finanziamenti procapite'),
+        ('top_collaboratori', 'Visualizza i 5 soggetti con cui collabora di più'),
+    )
+
+    INITIAL_TOPIC = 'miur', 'MIUR'
 
     def get_context_data(self):
-        context = super(SoggettoWidget, self).get_context_data()
+        context = Widget.get_context_data(self)
         if self.is_valid():
             cleaned_data = self.get_data()
             context['soggetto'] = request('soggetti/{0}'.format(cleaned_data['soggetto']))
-            context['top_progetti'] = request('progetti',
-                                              soggetto=cleaned_data['soggetto'],
-                                              order_by='-costo', page_size='5')
+            context['aggregati'] = context['soggetto'].pop('aggregati')
+            context['top_progetti'] = {'results': context['soggetto'].pop('top_progetti')}
+            context['top_collaboratori'] = {'results': context['soggetto'].pop('top_collaboratori')}
+            context['territori_piu_finanziati_pro_capite'] = {
+                'results': context['soggetto'].pop('territori_piu_finanziati_pro_capite')
+            }
         return context
 
-    def get_form(self):
-        form = super(SoggettoWidget, self).get_form()
-        form.fields['soggetto'] = forms.CharField()
-        return form
-
-    def get_initial(self):
-        initial = super(SoggettoWidget, self).get_initial()
-        initial['soggetto'] = 'miur'
-        return initial
+    def get_title(self):
+        return '{0}: {1}'.format(self.title, Soggetto.objects.get(slug=self.get_topic()).denominazione)
