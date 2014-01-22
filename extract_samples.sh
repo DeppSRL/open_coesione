18bddd9c38bb4d9c58866ebc3e016ffd90cb017c
Guglielmo <guglielmo.celata@gmail.com>
2014-01-22 11:50:16 +0100

    command to extract samples corrected

--- extract_samples.sh
+++ extract_samples.sh
@@ -2,9 +2,9 @@
 
 # parameters can be set here, to point to the correct position
 # and fix sample's size
-export DATA_ROOT=dati/dataset_latest # data path where original csv are (and produced sample will be)
-export DATE=20130228 # date used as suffix in csv filenames
-export N=5 # sample size
+export DATA_ROOT=dati/dataset_dev # data path where original csv are (and produced sample will be)
+export DATE=FSC0713_20131031 # suffix in csv filenames
+export N=500 # sample size
 
 pushd $DATA_ROOT
 
@@ -18,12 +18,12 @@ if [ ! -f localizzazioni_sorted.csv ]; then
     tail -n +2 localizzazioni_$DATE.csv | sort >> localizzazioni_sorted.csv
 fi
 
-if [ ! -f localizzazioni_sorted.csv ]; then
+if [ ! -f soggetti_sorted.csv ]; then
     head -n 1 soggetti_$DATE.csv > soggetti_sorted.csv
     tail -n +2 soggetti_$DATE.csv | sort >> soggetti_sorted.csv
 fi
 
-if [ ! -f localizzazioni_sorted.csv ]; then
+if [ ! -f pagamenti_sorted.csv ]; then
     head -n 1 pagamenti_$DATE.csv > pagamenti_sorted.csv
     tail -n +2 pagamenti_$DATE.csv | sort >> pagamenti_sorted.csv
 fi
@@ -37,6 +37,6 @@ head -n 1 pagamenti_$DATE.csv > pagamenti_sample.csv
 popd
 
 echo extract related records and append them to sample csv files
-python manage.py extractsamplesrelated --sample=progetti_sample.csv --type=loc localizzazioni_sorted.csv >> $DATA_ROOT/localizzazioni_sample.csv
-python manage.py extractsamplesrelated --sample=progetti_sample.csv --type=rec soggetti_sorted.csv >> $DATA_ROOT/soggetti_sample.csv
-python manage.py extractsamplesrelated --sample=progetti_sample.csv --type=pay pagamenti_sorted.csv >> $DATA_ROOT/pagamenti_sample.csv
+python manage.py extractsamplesrelated --data-root=$DATA_ROOT --sample=progetti_sample.csv --type=loc localizzazioni_sorted.csv >> $DATA_ROOT/localizzazioni_sample.csv
+python manage.py extractsamplesrelated --data-root=$DATA_ROOT --sample=progetti_sample.csv --type=rec soggetti_sorted.csv >> $DATA_ROOT/soggetti_sample.csv
+python manage.py extractsamplesrelated --data-root=$DATA_ROOT --sample=progetti_sample.csv --type=pay pagamenti_sorted.csv >> $DATA_ROOT/pagamenti_sample.csv
