c220ba409e3125905332d112326f22a8a007ec04
Guglielmo <guglielmo.celata@gmail.com>
2014-01-30 19:11:42 +0100

    subscription form prototype implemented

--- rubrica/forms.py
+++ rubrica/forms.py
@@ -15,8 +15,8 @@ class NLContactForm(forms.Form):
     title = forms.CharField(max_length=80, label='Qualifica', required=False)
     role = forms.CharField(max_length=80, label='Ruolo', required=False)
     user_type = forms.TypedChoiceField(choices=USER_TYPES, label='Tipologia di utente', empty_value= '-----')
-    notes = forms.CharField(widget=forms.Textarea, label='Note', required=False)
-    privacy = forms.BooleanField(required=True, help_text="Possiamo rivendere questi tuoi dati a chi ci pare?")
+    notes = forms.CharField(widget=forms.Textarea, label='Note', required=False, help_text="Note su questo contatto")
+    privacy = forms.BooleanField(required=True, help_text="Autorizzazione all'utilizzo dei dati personali.")
 
     def execute(self):
         email = self.cleaned_data.get('email','')
