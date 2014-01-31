28c7f893e36c20253f82215258f324e55027013e
Guglielmo <guglielmo.celata@gmail.com>
2014-01-31 17:28:50 +0100

    title and role fields not shown in newsletter subscription form; default value set to empty space for both of them

--- rubrica/forms.py
+++ rubrica/forms.py
@@ -12,8 +12,6 @@ class NLContactForm(forms.Form):
     first_name = forms.CharField(max_length= 80, label='Nome', required=False)
     last_name = forms.CharField(max_length= 80, label='Cognome', required=False)
     email = forms.EmailField(label="Email *")
-    title = forms.CharField(max_length=80, label='Qualifica', required=False)
-    role = forms.CharField(max_length=80, label='Ruolo', required=False)
     user_type = forms.TypedChoiceField(choices=USER_TYPES, label='Tipologia di utente *', empty_value= '-----')
     notes = forms.CharField(widget=forms.Textarea, label='Note', required=False, help_text="Spazio per delle note libere")
     privacy = forms.BooleanField(required=True, label='Autorizzazione all\'utilizzo dei dati personali *')
@@ -30,8 +28,8 @@ class NLContactForm(forms.Form):
             'last_name': self.cleaned_data.get('last_name',''),
         }
         iscrizione_dict = {
-            'title': self.cleaned_data.get('title', ''),
-            'role': self.cleaned_data.get('role', ''),
+            'title': '',
+            'role': '',
             'user_type': self.cleaned_data.get('user_type', ''),
             'notes': self.cleaned_data.get('notes', ''),
         }
