6692a39df600fa3a15ece163726d90d66e06bafd
Guglielmo <guglielmo.celata@gmail.com>
2014-06-03 11:54:44 +0200

    codice added to display fields in Programmi admin

--- progetti/admin.py
+++ progetti/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from progetti.models import *
 
 class ProgrammaAsseObiettivoAdmin(admin.ModelAdmin):
+    list_display = ['codice', 'descrizione']
     search_fields = ['descrizione', 'codice']
     list_filter = ('tipo_classificazione',)
 
 class ProgrammaLineaAzioneAdmin(admin.ModelAdmin):
     search_fields = ['descrizione', 'codice']
+    list_display = ['codice', 'descrizione']
     list_filter = ('tipo_classificazione',)
 
 class LocalizzazioneInline(admin.TabularInline):
