3ecc83bb0d25b42911073bdc18bf7071d1c9a1e5
sberna <sb.numi@libero.it>
2014-06-10 18:10:35 +0200

    Add accordion and AJAX loading to news

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from blog.views import *
 urlpatterns = patterns('',
     url(r'^$', BlogView.as_view(), name='blog_home'),
     url(r'^articolo/(?P<slug>[\w-]+)/$', BlogEntryView.as_view(), name='blog_item'),
+    url(r'^load/(?P<slug>[\w-]+)/$', blogEntryItem, name='blog_item_load'),
     # url(r'^tag/(?P<slug>[\w-]+)/$', BlogByTagView.as_view(), name='blog_by_tag'),
     # url(r'^date/(?P<date>[t|w|m|y])/$', BlogByDateView.as_view(), name='blog_by_date'),
 )
