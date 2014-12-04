ef6657c7fdae6a810b131f9f2c5f1819cf5d48a4
Guglielmo <guglielmo.celata@gmail.com>
2014-12-05 00:59:41 +0100

    open data page adjusted for 2014-08-31 data release

--- open_coesione/views.py
+++ open_coesione/views.py
@@ -326,7 +326,7 @@ class OpendataView(TemplateView):
     """
 
     # dates are attributes in the view, so that it can possibly be used in other views
-    data_date = '20140630'
+    data_date = '20140831'
     cipe_date = '20121231'
     spesa_date = '20141031'
 
