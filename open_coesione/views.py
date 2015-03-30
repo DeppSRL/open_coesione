5d2e4b203f6ff0d709069960ccf20a59a5d1844d
sberna <sb.numi@libero.it>
2015-03-30 16:41:58 +0200

    Removed caching of context in OpendataView class

--- open_coesione/views.py
+++ open_coesione/views.py
@@ -300,7 +300,6 @@ class ContactView(TemplateView):
 
 
 class OpendataView(TemplateView):
-    @cached_context
     def get_context_data(self, **kwargs):
         context = super(OpendataView, self).get_context_data(**kwargs)
 
