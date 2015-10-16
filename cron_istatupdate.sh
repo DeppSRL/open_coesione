9703931351538a2c79268765cc0b39014304bb32
guglielmo <guglielmo.celata@gmail.com>
2015-10-16 11:12:16 +0200

    cronjob to automate ISTAT data updates

--- cron_istatupdate.sh
+++ cron_istatupdate.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-pushd /home/open_coesione
-. /home/virtualenvs/open_coesione/bin/activate
+cd /home/open_coesione
+. /home/virtualenvs/open-coesione/bin/activate
  python manage.py istatupdate_new --verbosity=2 --collectstatic
 deactivate
-popd
 
