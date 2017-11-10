ae898f59281a82bb8576bba8bd5dacb414e89184
sberna <sb.numi@libero.it>
2017-11-10 16:03:42 +0100

    New Monitoraggi ASOC page/7

--- open_coesione/static/js/oc-asoc.js
+++ open_coesione/static/js/oc-asoc.js
@@ -4,5 +4,5 @@ $(document).ready(function() {
             url: '//cdn.datatables.net/plug-ins/1.10.15/i18n/Italian.json'
         },
         order: []
-    });
+    }).find('td:last-of-type a').css('white-space', 'nowrap');
 });
